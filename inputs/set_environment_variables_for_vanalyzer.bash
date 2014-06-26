ece8c3962b375da96e76832de5abceef603b558a
ellie-long <ellie@jlab.org>
2014-06-26 15:51:57 -0400

    Adding inputs, slowly

---
+++ inputs/set_environment_variables_for_vanalyzer.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+export DEBUG=0
+#export ROOTSYS=/u/apps/root/5.26-00-64bit/root
+source /u/apps/root/PRO/setroot_CUE
+#export ROOTSYS=/u/apps/root/5.34.01/root
+export ANALYZER=/u/home/ellie/analyzer-1.5.23
+export ELLANA=/work/halla/e05102/disk1/ellie/batch/inputs/ellana
+export PATH=${ROOTSYS}/bin:${ANALYZER}:${PATH}   
+export DB_DIR=${ANALYZER}/DB
+# export LD_LIBRARY_PATH=${ROOTSYS}/lib:${ANALYZER}/
+export EXPERIMENT=e05102
+export MYSQL=/apps/mysql
+export LD_LIBRARY_PATH=${ROOTSYS}/lib:${ANALYZER}:${MYSQL}/lib/mysql:${LD_LIBRARY_PATH}
